5f868a5e995023a032ff959291ddc8397ad47139
A. Tyler Benson <typrobenson@gmail.com>
2019-10-06 05:06:31 -0600

    Public_DIR

---
+++ app.js
@@ -0,0 +1,137 @@
+var port = process.env.PORT || 8080;
+const uri = "mongodb+srv://tbenson:fake123@dreamsnthings.mongodb.net/dnt?retryWrites=true&w=majority";
+
+var express 		= require('express'),
+	app 			= express(),
+	bodyParser 		= require("body-parser"), // Let us Read the REQ.
+	passport 		= require("passport"),
+	methodOverride	= require("method-override"), // Method - Override
+	expressSession 	= require("express-session"), 
+	LocalStrategy	= require("passport-local"), // Local Package passport
+	mongoPassport	= require("passport-local-mongoose"), // mongo local passport package
+	mongoose		= require("mongoose"); // DB ODM
+
+var User  			= require("./models/User"), // The User Model
+	Admin  			= require("./models/Admin"), // The Admin Model
+	Listener  		= require("./models/Listener"), // THe Listener Model
+	Creator 		= require("./models/Creator"), // The Creator Model
+	Track 			= require("./models/Track"), // The Track (Song) Model
+	Album 			= require("./models/Album"); // The Album Model
+
+//==================|
+// 	    DATABASE
+//   CONFIGURATION
+//==================|
+// Cloud Connection
+// mongoose.connect(uri , {
+// 	useUnifiedTopology: true, 
+// 	useNewUrlParser: true,
+// 	useCreateIndex: true
+// }).then(() => {
+// 	console.log("Connected to DB");
+// }).catch(err => {
+// 	console.log("ERROR! What is going on.... Details: " + err.message);
+// });
+// Local Connection
+mongoose.connect("mongodb://localhost:27017/dnt_db" , { useUnifiedTopology: true , useNewUrlParser: true });
+
+//==================|
+// 	  APPLICATION
+//   CONFIGURATION
+//==================|
+app.set('view engine' , 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serving the directory
+app.use(express.static(__dirname + "/public"));
+
+// Implementing Method Override 
+app.use(methodOverride("_method"));
+
+//================|
+// Passport Config
+//================|
+// app.use(expressSession ({
+// 	secret: "Utah is so beautiful!!!!",
+// 	resave: false,
+// 	saveUninitialized: false
+// }));
+
+// app.use(passport.initialize());
+// app.use(passport.session());
+// passport.use(new LocalStrategy(User.authenticate()));
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+
+
+//=======================|
+//    ROUTES 
+//=======================|
+/*HOME PAGE*/
+app.get("/" , (req , res) => {
+	res.render("home");
+});
+
+
+//=======================|
+//	Artist Route creator |
+//=======================|
+app.post("/creators" , (req , res) => {
+	console.log("Building a Mongo Creator JSON NODE Collection");
+	Creator.create(req.body.creator , (err , CreatedCreator) =>{
+		if(err){
+			console.log(err);
+			res.render("/");
+		} else {
+			res.redirect("/creators");
+		}
+	});
+
+	var creatorName 	= req.body.creatorName,
+		creatorGenre 	= req.body.creatorGenre,
+		creatorBIO		= req.body.creatorBIO,
+		creatorIMG		= req.body.creatorIMG;
+
+	var newCreator = {creatorName: creatorName, creatorGenre: creatorGenre, creatorBIO: creatorBIO, creatorIMG};
+
+	console.log("_________");
+	console.log(creatorName);
+	console.log(creatorBIO);
+	console.log(creatorIMG);
+	console.log(creatorGenre);
+	console.log("_________");
+
+	res.redirect("/creators/show");
+});
+
+app.get("/creators/show" , (req, res) => {
+	res.render("addCreator");
+});
+
+
+
+/* Registration Page */
+app.get("/signup" , (req , res) => {
+	res.render("signup");
+});
+
+app.post("/register" , (req , res) => {
+	var newUser = new User(req.body);
+})
+
+/* Login Route */
+app.get("/login" , (req , res) => {
+	res.render("login");
+});
+
+
+
+
+
+//=======================|
+// 	    	SERVER
+//  	 CONFIGURATION
+//=======================|
+app.listen(port , (req , res) =>  {
+	console.log("DreamsNStreams Initialized....");
+});
---
+++ readme.md
@@ -0,0 +1,20 @@
+# APPLICATION DEVELOPMENT
+	* DreamsNthings 
+		"A Music Production Streaming Platform Dreams N Things"
+
+## Routes
+* Index/Home
+* Discover
+* Signup -> Rego POST
+* Login
+* Logout
+
+### Todo
+	* Add RESTful Routing
+		** ! Stuck on Signup Route and Creating User and Passport.js 
+	* fix .active for .pills nav-items for main content to work ! GRRRR
+
+## Template Tasklist
+	* Mock up each use case on the main navigation
+	* Mock up login and registration
+	* Start piecing them together using Bootstrap classes - Piece by Piece.
